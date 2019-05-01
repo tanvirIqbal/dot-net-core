c4b097661641c60470175459460fc7f1a10c0efc
tanviriqbal <tanviriqbal4u@live.com>
2019-05-01 06:41:38 +0600

    Adding an actual service class

--- AspNetCoreTodo/Startup.cs
+++ AspNetCoreTodo/Startup.cs
@@ -43,7 +43,8 @@ namespace AspNetCoreTodo
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddSingleton<ITodoItemService, FakeTodoItemService>();
+            //services.AddSingleton<ITodoItemService, FakeTodoItemService>();
+            services.AddScoped<ITodoItemService, TodoItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
